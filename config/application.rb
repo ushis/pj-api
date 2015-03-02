b64c648f7e5ffa12d1adc80a0460a64bda88ff45
ushi <ushi@honkgong.info>
2015-03-02 03:31:01 +0100

    Use Sucker Punch for background jobs

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module PjApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Use sucker punch for background jobs
+    config.active_job.queue_adapter = :sucker_punch
   end
 end
