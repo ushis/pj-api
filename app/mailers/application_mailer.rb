b81b70bd70d98d8406108af43d560717ab415a03
ushi <ushi@honkgong.info>
2015-03-13 01:08:39 +0100

    Update mailer views

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,13 @@
 class ApplicationMailer < ActionMailer::Base
+  helper_method :format_datetime
 
   private
 
   def app_url(path)
     "#{ENV['APP_HOST']}/##{path}"
+  end
+
+  def format_datetime(datetime, time_zone)
+    datetime.in_time_zone(time_zone).strftime('%d %b %Y, %H:%M')
   end
 end
