0c0233d357b25be7202d4f89d1dfa5e28e0ec45b
ushi <ushi@honkgong.info>
2016-04-08 09:35:22 +0200

    airbrake config

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,8 +1,10 @@
-if !Rails.env.development? && !Rails.env.test?
-  Airbrake.configure do |config|
-    config.host = ENV['AIRBRAKE_HOST']
-    config.project_id = ENV['AIRBRAKE_ID']
-    config.project_key = ENV['AIRBRAKE_KEY']
-    config.ignore_environments = [:development, :test]
-  end
+Airbrake.configure do |c|
+  c.host = ENV['AIRBRAKE_HOST']
+  c.project_id = ENV.fetch('AIRBRAKE_ID', true)
+  c.project_key = ENV.fetch('AIRBRAKE_KEY', true)
+  c.root_directory = Rails.root
+  c.logger = Rails.logger
+  c.environment = Rails.env
+  c.ignore_environments = %w(development test)
+  c.blacklist_keys = [/password/i]
 end
