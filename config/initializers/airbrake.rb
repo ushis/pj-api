6263e9170b35da8c756f7ef675ae36a02d868550
ushi <ushi@honkgong.info>
2016-06-08 11:31:31 +0200

    do not require airbrake env vars

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |c|
   c.host = ENV['AIRBRAKE_HOST']
-  c.project_id = ENV.fetch('AIRBRAKE_ID', true)
-  c.project_key = ENV.fetch('AIRBRAKE_KEY', true)
+  c.project_id = ENV['AIRBRAKE_ID']
+  c.project_key = ENV['AIRBRAKE_KEY']
   c.root_directory = Rails.root
   c.logger = Rails.logger
   c.environment = Rails.env
